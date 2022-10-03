a55a2e9ffcf62dc3e0270e027ed6274fc0244a5d
Gabriel Lopes Rodrigues <gabriellopes@usp.br>
2022-10-03 17:54:32 -0300

    Agendando a geração de relatórios

---
+++ lib/rocketlivery/orders/report_runner.ex
@@ -0,0 +1,37 @@
+defmodule Rocketlivery.Orders.ReportRunner do
+  use GenServer
+
+  require Logger
+
+  alias Rocketlivery.Orders.Report
+
+  # Client
+
+  def start_link(_initial_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  # Server
+
+  @impl true
+  def init(state) do
+    Logger.info("Report Runner stated")
+    schedule_report_generation()
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:generate, state) do
+    Logger.info("Generating report...")
+
+    Report.create()
+    schedule_report_generation()
+
+    {:noreply, state}
+  end
+
+  def schedule_report_generation do
+    Process.send_after(self(), :generate, 1000 * 10)
+  end
+end
