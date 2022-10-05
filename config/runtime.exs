742f67a2e1a5a364745789f764ecef7b09220c38
Gabriel Lopes Rodrigues <gabriellopes@usp.br>
2022-10-05 19:48:50 -0300

    Adiciona informações do servidor ao runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -52,16 +52,9 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :rocketlivery, RocketliveryWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
-    secret_key_base: secret_key_base
+    server: true,
+    http: [port: {:system, "PORT"}],
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
 
   # ## Configuring the mailer
   #
